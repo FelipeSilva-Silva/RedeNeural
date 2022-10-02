111851b138c9d01a2d1cfd463116a2ba566c9623
vitordbo <vitorduarte177@gmail.com>
2022-10-01 22:38:45 -0300

    matrizes dos números

---
+++ src/Numeros.java
@@ -0,0 +1,145 @@
+public class Numeros{
+
+    public static void main(String[] args) {
+        
+        // zero
+        int zero[][] = { {1,0,0,0,1},{0,1,1,1,0},{0,1,1,1,0},{0,1,1,1,0},
+        {1,0,0,0,1} };
+
+        System.out.println("\nzero ");
+        for (int i=0; i< 5 ; i++)
+        {
+            for (int j=0; j < 5 ; j++)
+                System.out.print(zero[i][j] + " ");
+
+            System.out.println();
+        }
+
+        System.out.println("\n");
+        // um
+        int um[][] = { {1,1,0,0,0},{0,1,0,0,0},{0,1,0,0,0},{0,1,0,0,0},
+        {1,1,1,0,0} };
+
+        System.out.println("um ");
+        for (int i=0; i< 5 ; i++)
+        {
+            for (int j=0; j < 5 ; j++)
+                System.out.print(um[i][j] + " ");
+
+            System.out.println();
+        }
+
+        System.out.println("\n");
+        // dois
+        int dois[][] = { {0,2,2,2,0},{2,0,0,0,2},{0,0,0,2,0},{0,0,2,0,0},
+        {2,2,2,2,2} };
+
+        System.out.println("dois ");
+        for (int i=0; i< 5 ; i++)
+        {
+            for (int j=0; j < 5 ; j++)
+                System.out.print(dois[i][j] + " ");
+
+            System.out.println();
+        }
+
+        System.out.println("\n");
+        // tres
+        int tres[][] = { {3,3,3,3,0},{0,0,0,0,3},{0,0,0,3,0},{0,0,0,0,3},
+        {3,3,3,3,0} };
+
+        System.out.println("tres ");
+        for (int i=0; i< 5 ; i++)
+        {
+            for (int j=0; j < 5 ; j++)
+                System.out.print(tres[i][j] + " ");
+
+            System.out.println();
+        }
+
+        System.out.println("\n");
+        // quatro 
+        int quatro[][] = { {4,0,4,0,0},{4,0,4,0,0},{4,4,4,4,4},{0,0,0,4,0},
+        {0,0,0,4,0} };
+
+        System.out.println("quatro ");
+        for (int i=0; i< 5 ; i++)
+        {
+            for (int j=0; j < 5 ; j++)
+                System.out.print(quatro[i][j] + " ");
+
+            System.out.println();
+        }
+
+        System.out.println("\n");
+        // joao => primeiro numero = 5 
+        int cinco[][] = { {5,5,5,5,5},{5,0,0,0,0},{5,5,5,5,0},{0,0,0,5,0},
+        {5,5,5,5,0} };
+
+        System.out.println("cinco ");
+        for (int i=0; i< 5 ; i++)
+        {
+            for (int j=0; j < 5 ; j++)
+                System.out.print(cinco[i][j] + " ");
+
+            System.out.println();
+        }
+
+        System.out.println("\n");
+        // seis
+        int seis[][] = { {0,6,6,6,6},{6,0,0,0,0},{6,6,6,6,0},{6,0,0,0,6},
+        {0,6,6,6,0} };
+
+        System.out.println("seis ");
+        for (int i=0; i< 5 ; i++)
+        {
+            for (int j=0; j < 5 ; j++)
+                System.out.print(seis[i][j] + " ");
+
+            System.out.println();
+        }
+
+        System.out.println("\n");
+        // sete
+        int sete[][] = { {7,7,7,7,7},{0,0,0,0,7},{0,0,0,7,0},{0,0,7,0,0},
+        {0,0,7,0,0} };
+
+        System.out.println("sete ");
+        for (int i=0; i< 5 ; i++)
+        {
+            for (int j=0; j < 5 ; j++)
+                System.out.print(sete[i][j] + " ");
+
+            System.out.println();
+        }
+
+        System.out.println("\n");
+        // oito => segundo nome => vitor
+        int oito[][] = { {0,8,8,8,0},{8,0,0,0,8},{0,8,8,8,0},{8,0,0,0,8},
+        {0,8,8,8,0} };
+
+        System.out.println("oito ");
+        for (int i=0; i< 5 ; i++)
+        {
+            for (int j=0; j < 5 ; j++)
+                System.out.print(oito[i][j] + " ");
+
+            System.out.println();
+        }
+
+        System.out.println("\n");
+        // nove
+        int nove[][] = { {0,9,9,9,0},{9,0,0,0,9},{0,9,9,9,9},{0,0,0,0,9},
+        {9,9,9,9,9} };
+
+        System.out.println("nove ");
+        for (int i=0; i< 5 ; i++)
+        {
+            for (int j=0; j < 5 ; j++)
+                System.out.print(nove[i][j] + " ");
+
+            System.out.println();
+        }
+    }
+  
+}
